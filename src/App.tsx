3b141005324ee7e1db6a9067073253113f40754e
hhwaaaaa1 <hhwaaaaa1@naver.com>
2021-02-06 20:23:59 +0900

    Node.js 연동

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
 import Calendar from "@/components/Calendar";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [response, setResponse] = useState<unknown>(null);
+
+  useEffect(() => {
+    testAPI().then((res) => setResponse(res));
+  }, []);
+
+  async function testAPI() {
+    const res = await fetch("/api");
+    const body = await res.json();
+    return body;
+  }
+
   return (
     <div>
+      {JSON.stringify(response)}
       <Calendar />
     </div>
   );
